737423c46cc4666109fa2b3d499f351455a21b34
SullyvanMoura <sullyvanmoura@hotmail.com>
2024-05-08 17:41:50 -0300

    room manager logs

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -12,15 +12,6 @@ public class Server {
     public static void main(String[] args) {
 
     	Database.createTables();
-    	//System.out.println(Database.insertReservation("20200007457", "lab306", 2024, 07, 10, 10));
-//    	System.out.println(Database.removeReservation("20200007457", "lab306", 2024, 07, 10, 10));
-//    	boolean searchReservation = Database.isRoomReserved("lab306", 2024, 07, 10, 10);
-//    	
-//    	System.out.println(searchReservation);
-    	
-//    	for (Reservation r : Database.getReservationByRoom("lab306")) {
-//    		System.out.println(String.format("RoomId: %s| UserId: %s| Date: %s", r.getRoomId(), r.getUserId(), r.getDateTime()));
-//    	}
 
     	//EXPÕE ONLINE
         try {
@@ -39,6 +30,5 @@ public class Server {
             System.err.println("Server exception: " + e.toString());
             e.printStackTrace();
         }
-        //System.exit(0);
     }
 }
--- src/main/java/server/impls/RoomManagerImpl.java
+++ src/main/java/server/impls/RoomManagerImpl.java
@@ -22,6 +22,8 @@ public class RoomManagerImpl extends UnicastRemoteObject implements RoomManager 
 	public String createReservation(String userId, String roomId, int year, int month, int day, int hour)
 			throws RemoteException {
 		
+		System.out.println(String.format("LOG: Insert reservation process required. USER_ID [%s] - ROOM_ID [%s] - DATE_TIME [%sh %d/%s/%s]", userId, roomId, hour, day, month, year));
+
 		return Database.insertReservation(userId, roomId, year, month, day, hour);
 	}
 
@@ -29,31 +31,39 @@ public class RoomManagerImpl extends UnicastRemoteObject implements RoomManager 
 	public String removeReservation(String userId, String roomId, int year, int month, int day, int hour)
 			throws RemoteException {
 		
+		System.out.println(String.format("LOG: Remove reservation process required. USER_ID [%s] - ROOM_ID [%s] - DATE_TIME [%sh %d/%s/%s]", userId, roomId, hour, day, month, year));
+		
 		return Database.removeReservation(userId, roomId, year, month, day, hour);
 	}
 
 	@Override
 	public String checkReservation(String roomId, int year, int month, int day, int hour) throws RemoteException {
 		
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String formattedDateTime = LocalDateTime.of(year, month, day, hour, 0).format(formatter);
+		System.out.println(String.format("LOG: Check reservation process required ROOM_ID [%s] - DATE_TIME [%sh %d/%s/%s]", roomId, hour, day, month, year));
 		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		String formattedDateTime = LocalDateTime.of(year, month, day, hour, 0).format(formatter);
+
 		if (Database.isRoomReserved(roomId, year, month, day, hour)) {
-			
+
 			return String.format("Room [%s] is RESERVED at [%s]", roomId, formattedDateTime);
 		}
-		
+
 		return String.format("Room [%s] is FREE at [%s]", roomId, formattedDateTime);
 	}
 
 	@Override
 	public List<Reservation> checkRoomReservations(String roomId) throws RemoteException {
 		
+		System.out.println(String.format("LOG: Check room's reservations process required. ROOM_ID [%s]", roomId));
+
 		return Database.getReservationByRoom(roomId);
 	}
 
 	@Override
 	public List<Reservation> checkUserReservations(String userId) throws RemoteException {
+		
+		System.out.println(String.format("LOG: Check user's reservations process required. USER_ID [%s]", userId));
 		
 		return Database.getReservationByUser(userId);
 	}
